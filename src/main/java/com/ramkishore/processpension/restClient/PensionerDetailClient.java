78ae6d8673699210df95ef08bc8401ff87eb4cff
RAMKISHORE DYAVANPALLY <48268023+Ramkishore417@users.noreply.github.com>
2022-08-22 12:39:43 +0530

    paths updated.

--- src/main/java/com/ramkishore/processpension/restClient/PensionerDetailClient.java
+++ src/main/java/com/ramkishore/processpension/restClient/PensionerDetailClient.java
@@ -8,11 +8,11 @@ import org.springframework.web.bind.annotation.*;
 @FeignClient(name="pensioner-detail", url = "${PENSIONER_DETAIL_URI : http://localhost:8002}")
 public interface PensionerDetailClient {
 
-	@GetMapping("/PensionerDetailByAadhaar/{aadhaarNumber}")
+	@GetMapping("/api/pensionerdetail/PensionerDetailByAadhaar/{aadhaarNumber}")
 	public PensionerDetail findByAadhaarNumber(@RequestHeader("Authorization") String token,
 											   @PathVariable String aadhaarNumber);
 
-	@PostMapping("/saveTransactionDetail")
+	@PostMapping("/api/pensionerdetail/saveTransactionDetail")
 	public boolean savePensionTransactionDetail(@RequestHeader("Authorization") String token,
 												@RequestBody PensionTransactionDetail transactionDetail);
 
